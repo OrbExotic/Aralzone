a4474e4e7412a72642ee1b0ded149924e39591c0
ajacos <acosta.aaron@psdqatar.com>
2025-07-01 17:16:59 +0300

    Added project

--- public/lib/supabaseClient.js
+++ public/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = process.env.https://izayuejeigaqvqffscpn.supabase.co
+const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6YXl1ZWplaWdhcXZxZmZzY3BuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3NzA2ODQsImV4cCI6MjA2NjM0NjY4NH0.fvAATp7FWNY1OR7p3BDiNsDm4i0ZPyEzOju1bMPEwPs
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
